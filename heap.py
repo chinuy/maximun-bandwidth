823ed5dbb173177d6b60b6095694d4438b2a75b6
Chih-Peng Wu <chinuy@gmail.com>
2014-03-28 17:32:00 -0500

    1. root point set index to -1; 2. __contains__()

--- heap.py
+++ heap.py
@@ -5,10 +5,17 @@ class Heap:
 
     def __init__(self):
         # array item type: tuple, (id, value)
-        self.array = [(0,-1)]
+        self.array = [(-1,-1)]
 
     def __len__(self):
         return len(self.array) -1
+
+    def __contains__(self, item):
+        "Return True if item id is in"
+        for elm in self.array:
+            if item is elm[0]:
+                return True
+        return False
 
     def getMin(self):
         return self.array[1]
@@ -27,7 +34,7 @@ class Heap:
             self.adjust(int(i_parent/2), i_parent)
 
     def delete_root(self):
-        if len(self) < 0:
+        if len(self) < 1:
             return
 
         root_index = 1
@@ -61,7 +68,7 @@ class MaxHeap(Heap):
 
     def __init__(self):
         # array item type: tuple, (id, value)
-        self.array = [(0,sys.maxint)]
+        self.array = [(-1,sys.maxint)]
 
     def getMin(self):
         raise Exception("MaxHeap doesn't support getMin()")
@@ -100,7 +107,10 @@ def main():
     for i in x:
         h.insert(i)
     print h.array
-    print "DELETE"
+    print "-- Test contains()"
+    print 10 in h
+    print 1 in h
+    print "-- DELETE"
     for i in range(len(h)):
         h.delete_root()
     print h.array
