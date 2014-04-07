60411f4ea61f0cd7a2209ea22a0d326998598141
Chih-Peng Wu <chinuy@gmail.com>
2014-04-07 15:23:19 -0500

    testing

--- testing.py
+++ testing.py
@@ -3,25 +3,41 @@ import routing
 import graph
 from routing import randomSourceSink
 
+def array_init(m, init_value):
+    matrix = list()
+    for i in range(m):
+        row = list()
+        for j in range(m):
+           row.append(init_value)
+        matrix.append(row)
+    return matrix
+
 @profile.timeit
 def problem_solver(algorithm, problem_graph, problem_source_sink):
     problem = algorithm(problem_graph)
     problem.setSourceSink(*problem_source_sink)
     problem.solve()
+    return problem.count
 
 def main():
 
     num_vertex = 5000
+    NUM_ITERATION = 5
     algorithms = [routing.Dijkstra_without_heap, routing.Dijkstra_with_heap, routing.Kruskal_with_heap]
 
-    for i in range(5):
+    summary = array_init(3, 0)
+    for i in range(NUM_ITERATION):
         problem_graphs = [graph.Graph_six_degree(num_vertex), graph.Graph_random_connect20(num_vertex)]
-        for problem_g in problem_graphs:
+        for j in range(len(problem_graphs)):
+            problem_g = problem_graphs[j]
             problem_source_sink = randomSourceSink(num_vertex)
             problem_g.amend_gap(*problem_source_sink)
-            for algo in algorithms:
-                solver = problem_solver(algo, problem_g, problem_source_sink)
-                print problem_solver.times
+            for k in range(len(algorithms)):
+                solver = problem_solver(algorithms[k], problem_g, problem_source_sink)
+                summary[j][k] += solver
+    for j in range(3):
+        for k in range(3):
+            print summary[j][k]/NUM_ITERATION
 
 if __name__ == '__main__':
     main()
